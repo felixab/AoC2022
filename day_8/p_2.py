3989fb83b911bb50a5169c68b8f12e2f7f464b03
Felix-Antoine Bourbonnais <felix16.82@hotmail.fr>
2022-12-09 21:47:50 -0500

    day 8 p_2

--- day_8/p_2.py
+++ day_8/p_2.py
@@ -1,2 +1,59 @@
 with open("input1.txt") as f:
     lines = f.readlines()
+    trees = []
+    tree_set = set()
+    res = 0
+    for line in lines:
+        tree_row = []
+        line = list(line.strip())
+        for tree in line:
+            tree_row.append(int(tree))
+        trees.append(tree_row)
+
+    for j in range(len(trees)):
+        for i in range(len(trees[0])):
+            current = trees[j][i]
+            # l, r, t ,b
+            scenics = [0, 0, 0, 0]
+            k = i - 1
+            while k >= 0:
+                if trees[j][k] < current:
+                    scenics[0] += 1
+                else:
+                    scenics[0] += 1
+                    break
+                k -= 1
+            k = i + 1
+            while k < len(trees[0]):
+                if trees[j][k] < current:
+                    scenics[1] += 1
+                else:
+                    scenics[1] += 1
+                    break
+                k += 1
+            l = j - 1
+            while l >= 0:
+                if trees[l][i] < current:
+                    scenics[2] += 1
+                else:
+                    scenics[2] += 1
+                    break
+                l -= 1
+            l = j + 1
+            while l < len(trees):
+                if trees[l][i] < current:
+                    scenics[3] += 1
+                else:
+                    scenics[3] += 1
+                    break
+                l += 1
+            score = 0
+            for scenic in scenics:
+                if score == 0:
+                    score += scenic
+                else:
+                    score *= scenic
+            if score > res:
+                res = score
+
+print(res)
---
+++ day_9/p_2.py
@@ -0,0 +1,52 @@
+import math
+
+with open("input1.txt") as f:
+    lines = f.readlines()
+    visited = set()
+    visited.add((0, 0))
+    t_x = 0  # probably a list of coordinates for each knot
+    t_y = 0
+    h_x = 0
+    h_y = 0
+    l_h_x = 0
+    l_h_y = 0
+    lastDir = None
+    for line in lines:
+        line = line.strip().split(" ")
+        direction = line[0]
+        steps = int(line[1])
+        for step in range(steps):
+            if direction == "L":
+                h_x -= 1
+                # distance bigger than 2
+                if (math.sqrt((t_x - h_x) ** 2 + (t_y - h_y) ** 2) >= 2):
+                    visited.add((l_h_x, l_h_y))
+                    t_x = l_h_x
+                    t_y = l_h_y
+                l_h_x = h_x
+
+            if direction == "R":
+                h_x += 1
+                # distance bigger than 2
+                if (math.sqrt((t_x - h_x) ** 2 + (t_y - h_y) ** 2) >= 2):
+                    visited.add((l_h_x, l_h_y))
+                    t_x = l_h_x
+                    t_y = l_h_y
+                l_h_x = h_x
+            if direction == "U":
+                h_y += 1
+                # distance bigger than 2
+                if (math.sqrt((t_x - h_x) ** 2 + (t_y - h_y) ** 2) >= 2):
+                    visited.add((l_h_x, l_h_y))
+                    t_x = l_h_x
+                    t_y = l_h_y
+                l_h_y = h_y
+            if direction == "D":
+                h_y -= 1
+                # distance bigger than 2
+                if (math.sqrt((t_x - h_x) ** 2 + (t_y - h_y) ** 2) >= 2):
+                    visited.add((l_h_x, l_h_y))
+                    t_x = l_h_x
+                    t_y = l_h_y
+                l_h_y = h_y
+print(len(visited))
